f4ac766461776505a98cb259374c97287153155f
ramessh-thapa <62200333+ramessh-thapa@users.noreply.github.com>
2023-07-17 15:23:24 -0500

    Added Avatar service

---
+++ AvatarService/src/main/java/com/example/demo/controller/AvatarController.java
@@ -0,0 +1,47 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.AvatarDTO;
+import com.example.demo.service.AvatarService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("avatars")
+public class AvatarController {
+    private final AvatarService avatarService;
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<AvatarDTO> getAll(){
+        return avatarService.findAll();
+    }
+
+    @GetMapping("{avatarId}")
+    @ResponseStatus(HttpStatus.OK)
+    public AvatarDTO getById(@PathVariable("avatarId") String id){
+        return avatarService.getById(id);
+    }
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public AvatarDTO save(@RequestBody AvatarDTO dto){
+        return avatarService.save(dto);
+    }
+
+    @PutMapping("{avatarId}")
+    @ResponseStatus(HttpStatus.OK)
+    public AvatarDTO update(@RequestBody AvatarDTO dto, @PathVariable("avatarId") String id){
+        return avatarService.update(dto, id);
+    }
+
+    @DeleteMapping("{avatarId}")
+    @ResponseStatus(HttpStatus.OK)
+    public void delete(@PathVariable("avatarId") String id){
+         avatarService.delete(id);
+    }
+
+
+}
---
+++ AvatarService/src/main/java/com/example/demo/dto/AvatarDTO.java
@@ -0,0 +1,17 @@
+package com.example.demo.dto;
+
+public class AvatarDTO {
+    private String id;
+    private String head;
+    private String hair;
+    private String eye;
+    private String eyebrow;
+    private String nose;
+    private String mouth;
+    private String ears;
+    private String body;
+    private String hat;
+    private String top;
+    private String topColor;
+    private String hatColor;
+}

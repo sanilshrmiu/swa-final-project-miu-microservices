45f96b98742ec7f836bda99a893597697b18f48c
iqhossan <hossan.iqbal@gmail.com>
2023-07-18 01:15:07 -0500

    adding school and update teacher & student-service

--- TeacherService/src/main/java/com/TeacherService/controller/TeacherController.java
+++ TeacherService/src/main/java/com/TeacherService/controller/TeacherController.java
@@ -3,6 +3,7 @@ package com.TeacherService.controller;
 import com.TeacherService.domain.Teacher;
 import com.TeacherService.service.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,31 +11,37 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/teacher")
+
 public class TeacherController {
 
     @Autowired
     TeacherService teacherService;
+    @Autowired
+    SchoolFeignClient schoolFeignClient;
 
-    @PostMapping("/add")
-    ResponseEntity<Teacher> addTeacher(@RequestBody Teacher teacher){
+    @PostMapping("/teacher/add")
+    ResponseEntity<?> addTeacher(@RequestBody Teacher teacher){
+        Boolean schoolCheck =  schoolFeignClient.getSchoolId(teacher.getSchoolId());
+        if(!schoolCheck) {
+            return new ResponseEntity<String>("School Id does not exist", HttpStatus.OK);
+        }
         Teacher response = teacherService.addTeacher(teacher);
         return new ResponseEntity<Teacher>(response, HttpStatus.OK);
     }
 
-    @PutMapping("/update")
+    @PutMapping("/teacher/update")
     ResponseEntity<?> updateTeacher(@RequestBody Teacher teacher){
         Teacher response = teacherService.updateTeacher(teacher);
         return new ResponseEntity<Teacher>(response, HttpStatus.OK);
     }
 
-    @DeleteMapping("/delete/{teacherId}")
+    @DeleteMapping("/teacher/delete/{teacherId}")
     ResponseEntity<String> deleteTeacher(@PathVariable Long teacherId){
         String response = teacherService.deleteTeacher(teacherId);
         return new ResponseEntity<String>(response, HttpStatus.OK);
     }
 
-    @GetMapping("/get/{teacherId}")
+    @GetMapping("/teacher/get/{teacherId}")
     ResponseEntity<?> getTeacherById(@PathVariable Long teacherId){
         Teacher response = teacherService.getTeacherById(teacherId);
       // return new ResponseEntity<Teacher>(response, HttpStatus.OK);
@@ -46,11 +53,18 @@ public class TeacherController {
         }
     }
 
-    @GetMapping("/view")
+    @GetMapping("/teacher/view")
     ResponseEntity<List<Teacher>> getAllTeacher(){
         List<Teacher> response = teacherService.getAllTeacher();
         if(response!=null) return new ResponseEntity<List<Teacher>>(response, HttpStatus.OK);
         else return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+    }
+
+
+    @FeignClient(name = "SchoolService")
+    interface SchoolFeignClient{
+        @RequestMapping("/school/verifyReference/{schoolId}")
+        public Boolean getSchoolId(@PathVariable("schoolId") Long schoolId);
     }
 
 }
